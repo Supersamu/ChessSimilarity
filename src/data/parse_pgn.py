44323077160132a4b11777ad3ef27771a037af61
Supersamu <samuel.schueler@web.de>
2025-08-23 16:57:47 +0200

    refactor and fix not saving initial position

--- src/data/parse_pgn.py
+++ src/data/parse_pgn.py
@@ -11,32 +11,32 @@ import json
 import io
 from lichess_data_loading.gm_usernames import Lichess_names
 
+def board_to_3d_array(board: chess.Board) -> np.ndarray:
+    """
+    Convert a chess board to a 3D numpy array.
+    """
+    board_array = np.zeros((12, 8, 8), dtype=int)  # channel-first
+    piece_map = {
+        'P': 5, 'N': 4, 'B': 3, 'R': 2, 'Q': 1, 'K': 0,  # White pieces
+        'p': 6, 'n': 7, 'b': 8, 'r': 9, 'q': 10, 'k': 11  # Black pieces
+    }
+    for square in chess.SQUARES:
+        piece = board.piece_at(square)
+        if piece:
+            row, col = divmod(square, 8)
+            board_array[piece_map[piece.symbol()], row, col] = 1
+    return board_array
 
 
 def pgn_to_3d_arrays(pgn):
     game = chess.pgn.read_game(io.StringIO(pgn))
     board = game.board()
-    board_states = []
-
-    # Map pieces to unique integers
-    piece_map = {
-        'P': 5, 'N': 4, 'B': 3, 'R': 2, 'Q': 1, 'K': 0,  # White pieces
-        'p': 6, 'n': 7, 'b': 8, 'r': 9, 'q': 10, 'k': 11  # Black pieces
-    }
-    # For saving the boardstates as tensors, we are now able to flip
-    # the tensor along the first dimension to get the game from the the other direction
+    board_states = [board_to_3d_array(board)]
     
     # Iterate through moves
     for move in game.mainline_moves():
         board.push(move)
-        # Create an 8x8 array for the current board state
-        board_array = np.zeros((12, 8, 8), dtype=int)  # channel-first
-        for square in chess.SQUARES:
-            piece = board.piece_at(square)
-            if piece:
-                row, col = divmod(square, 8)
-                board_array[piece_map[piece.symbol()], row, col] = 1
-        board_states.append(board_array)
+        board_states.append(board_to_3d_array(board))
 
     return board_states
 
