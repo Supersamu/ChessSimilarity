a9bd9ee9a9a7c39fc012bb529a9c0675fe2b2bdc
Supersamu <samuel.schueler@web.de>
2025-08-24 15:07:59 +0200

    add nested validation split to mlflow utils

---
+++ src/mlflow_demo/utils.py
@@ -0,0 +1,28 @@
+import numpy as np
+import torch
+from torch.utils.data import Dataset, DataLoader
+from typing import List, Tuple
+from functools import reduce
+from collections import defaultdict
+from src.model_utils.data_loader import create_dataloaders, ChessDataset
+
+
+def create_train_val_test_split(player_names: List[str], batch_size: int, train_val_vs_test_split: float, 
+                                train_vs_val_split: float, random_seed: int) -> Tuple[DataLoader, DataLoader, DataLoader, int]:
+    """
+    Create train, validation, and test DataLoaders.
+
+    player_names: List[str]: List of player names.
+    batch_size: int: Batch size for DataLoaders.
+    train_val_vs_test_split: float: Split ratio for train/val vs test.
+    train_vs_val_split: float: Split ratio for train vs validation.
+    random_seed: int: Random seed for reproducibility.
+
+    Returns:
+    Tuple[DataLoader, DataLoader, DataLoader, int]: Train, validation, and test DataLoaders, and number of classes.
+    """
+    _, test_loader, _, train_indices, _ = create_dataloaders(player_names=player_names, batch_size=batch_size, 
+                                                             train_split=train_val_vs_test_split, random_seed=random_seed)
+    train_loader, val_loader, num_classes, _, _ = create_dataloaders(player_names=player_names, available_indices=train_indices, 
+                                                                     batch_size=batch_size, train_split=train_vs_val_split, random_seed=random_seed)
+    return train_loader, val_loader, test_loader, num_classes
